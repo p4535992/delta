986a602376a583242b61b12b0e1c146df7b9b16e
Delta <Delta>
2014-09-09 13:35:58 +0300

    ﻿Delta versioon 3.6.30.232
    Tarnega tulevad kaasa järgmised täiendused:
    
    Delta 3.6.30 haru obfuskeerimise skriptid, lisatud struktuuriüksustele vastavate kasutajagruppide obfuskeerimine.
    
    Administraatorile:
    
    	Täiendatud on olemasolevaid obfuskeerimise skripte ning lisatud eraldi skript, mis võimaldab obfuskeerida ainult struktuuriüksused andmebaasis, kus ülejäänud andmed on eelnevalt obfuskeeritud.
    
    	Eelnevalt obfuskeeritud baasis täiendava struktuuriüksuste obfuskeerimise läbiviimiseks tuleb käivitada skript common/etc/replaceSensitiveDataWithDummies.sql. Skriptide kasutamiseks ei ole vaja rakendust uuesti ehitada, kuid obfuskeerimise käivitamiseks peab rakendus olema seisatud.
    
    	Kogu andmebaasi uuesti obfuskeerimiseks järgida Delta versiooniga 3.6.30.231 kaasasolevat obfuskeermise juhendit. Skriptide kasutuses midagi ei muutu, struktuuriüksuste obfuskeerimine teostatakse sel juhul koos ülejäänud obfuskeerimistega.

---
+++ common/etc/replaceUserGroupsWithDummies.sql
@@ -0,0 +1,8 @@
+-- Skript obfuskeerib public.alf_authority tabelis kasutajagruppide (struktuuriüksuste) andmed
+update public.alf_authority set 
+	authority =
+	concat('GROUP_', case when position(',' in authority) < 1 then 'obfuskeeritud' 
+		else repeat('obfuskeeri, tud', array_length(regexp_split_to_array(authority, '(,)'), 1)) end, id ),
+	crc = obfuskeeritud.crc32(concat('GROUP_', case when position(',' in authority) < 1 then 'obfuskeeritud' 
+		else repeat('obfuskeeri, tud', array_length(regexp_split_to_array(authority, '(,)'), 1)) end, id ))
+	where authority like 'GROUP_%' and authority not in ('GROUP_ALFRESCO_ADMINISTRATORS', 'GROUP_ARCHIVISTS', 'GROUP_DOCUMENT_MANAGERS', 'GROUP_EVERYONE', 'GROUP_SUPERVISION');
