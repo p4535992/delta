39c8c2783929af23cf8e4a93fa7e83501a7d402a
Aleksandr Brukvin <Aleksandr.Brukvin@just.ee>
2018-03-05 12:42:46 +0200

    Cherry pick

--- source/java/ee/webmedia/alfresco/status/DependencyCheckers/XTeeDependencyChecker.java
+++ source/java/ee/webmedia/alfresco/status/DependencyCheckers/XTeeDependencyChecker.java
@@ -35,44 +35,13 @@ public class XTeeDependencyChecker extends DependencyChecker{
 		this.Producer = producer;
 		try{
 			URL url = new URL(this.Uri);
-			this.XTeeSecurityServer = url.toString();
+			this.XTeeSecurityServer = String.format("%s://%s", url.getProtocol(), url.getAuthority());
 		}catch(Exception ex){}
 		
 		// pathi's peab olema ka producer ..
-		this.Uri = String.format("%s@%s", this.Producer, this.XTeeSecurityServer);
+		this.Uri = String.format("%s@%s/cgi-bin/consumer_proxy", this.Producer, this.XTeeSecurityServer);
 	}
 	
-	public Boolean testDHXConnection(){
-		try {
-			URL v6Url = new URL(this.XTeeSecurityServer);
-			URL DHXUrl = new URL(String.format("%s://%s/dhx-adapter-server/health", v6Url.getProtocol(), v6Url.getAuthority()));
-			HttpURLConnection conn = (HttpURLConnection) DHXUrl.openConnection();
-			conn.setRequestMethod("GET");
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		
-			String line = rd.readLine();
-			Integer statusCode = conn.getResponseCode(); 
-			String statusMesg = conn.getResponseMessage();
-			
-			rd.close();
-			
-			Status = STATUS_NOK;
-			if(statusCode.equals(200)){
-				Status = STATUS_OK;
-				StatusMsg = "Saatmisviis: DHX " + this.XTeeSecurityServer;
-			    return true;
-			}
-									
-			// ..
-			StatusMsg = statusCode + " " + statusMesg;
-		    return false;
-		    
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return false;
-	}
 	
 	
 	/**
@@ -82,7 +51,7 @@ public class XTeeDependencyChecker extends DependencyChecker{
 		try{
 			
 			// wsdl url ..
-			URL url = new URL(this.XTeeSecurityServer);
+			URL url = new URL(String.format("%s/cgi-bin/uriproxy?producer=%s",this.XTeeSecurityServer, this.Producer));
 			
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
@@ -98,19 +67,20 @@ public class XTeeDependencyChecker extends DependencyChecker{
 			Status = STATUS_NOK;
 			if(statusCode.equals(200)){
 				Status = STATUS_OK;
-				StatusMsg = "Saatmisviis: V6 " + this.XTeeSecurityServer;
+				StatusMsg = this.XTeeSecurityServer;
 			    return true;
 			}
 									
 			// ..
 			StatusMsg = statusCode + " " + statusMesg;
-		    return testDHXConnection();
-		}catch(Exception ex){	
-			
+		    return false;
+		}catch(Exception ex){
 			StatusMsg = ex.getMessage();
-				
-			}
+			
 			Status = STATUS_NOK;
-		    return testDHXConnection();
+		    return false;
 		}
     }
+
+
+}
