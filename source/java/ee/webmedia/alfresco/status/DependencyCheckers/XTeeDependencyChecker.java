fad8681785e5e263d18c389f71ac523aeece6b6b
Aare Puusepp <aare.puusepp@gmail.com>
2018-05-28 14:07:21 +0300

    Mesting fixed from 5.2.3 branch:
    Tarnega tulevad kaasa järgmised täiendused ja parandused:
    -  DELTA-1507 – Delta statuslehe vigade parandamine

--- source/java/ee/webmedia/alfresco/status/DependencyCheckers/XTeeDependencyChecker.java
+++ source/java/ee/webmedia/alfresco/status/DependencyCheckers/XTeeDependencyChecker.java
@@ -35,13 +35,44 @@ public class XTeeDependencyChecker extends DependencyChecker{
 		this.Producer = producer;
 		try{
 			URL url = new URL(this.Uri);
-			this.XTeeSecurityServer = String.format("%s://%s", url.getProtocol(), url.getAuthority());
+			this.XTeeSecurityServer = url.toString();
 		}catch(Exception ex){}
 		
 		// pathi's peab olema ka producer ..
-		this.Uri = String.format("%s@%s/cgi-bin/consumer_proxy", this.Producer, this.XTeeSecurityServer);
+		this.Uri = String.format("%s@%s", this.Producer, this.XTeeSecurityServer);
 	}
 	
+	public Boolean testDHXConnection(){
+		try {
+			URL v6Url = new URL(this.XTeeSecurityServer);
+			URL DHXUrl = new URL(String.format("%s://%s/dhx-adapter-server/health", v6Url.getProtocol(), v6Url.getAuthority()));
+			HttpURLConnection conn = (HttpURLConnection) DHXUrl.openConnection();
+			conn.setRequestMethod("GET");
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		
+			String line = rd.readLine();
+			Integer statusCode = conn.getResponseCode(); 
+			String statusMesg = conn.getResponseMessage();
+			
+			rd.close();
+			
+			Status = STATUS_NOK;
+			if(statusCode.equals(200)){
+				Status = STATUS_OK;
+				StatusMsg = "Saatmisviis: DHX " + this.XTeeSecurityServer;
+			    return true;
+			}
+									
+			// ..
+			StatusMsg = statusCode + " " + statusMesg;
+		    return false;
+		    
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
 	
 	
 	/**
@@ -51,7 +82,7 @@ public class XTeeDependencyChecker extends DependencyChecker{
 		try{
 			
 			// wsdl url ..
-			URL url = new URL(String.format("%s/cgi-bin/uriproxy?producer=%s",this.XTeeSecurityServer, this.Producer));
+			URL url = new URL(this.XTeeSecurityServer);
 			
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
@@ -67,20 +98,19 @@ public class XTeeDependencyChecker extends DependencyChecker{
 			Status = STATUS_NOK;
 			if(statusCode.equals(200)){
 				Status = STATUS_OK;
-				StatusMsg = this.XTeeSecurityServer;
+				StatusMsg = "Saatmisviis: V6 " + this.XTeeSecurityServer;
 			    return true;
 			}
 									
 			// ..
 			StatusMsg = statusCode + " " + statusMesg;
-		    return false;
-		}catch(Exception ex){
-			StatusMsg = ex.getMessage();
+		    return testDHXConnection();
+		}catch(Exception ex){	
 			
+			StatusMsg = ex.getMessage();
+				
+			}
 			Status = STATUS_NOK;
-		    return false;
+		    return testDHXConnection();
 		}
     }
-
-
-}
