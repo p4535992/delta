7ae9e827c507ffb2f4cad9bec7ff2dcf1425ebc3
aleksandr.brukvin <aleksandr.brukvin@A13062.just.sise>
2018-08-30 08:57:47 +0300

    DELTA-1393

--- common/web/scripts/jquery/jquery.autocomplete.js
+++ common/web/scripts/jquery/jquery.autocomplete.js
@@ -10,6 +10,10 @@ jQuery.autocomplete = function(input, options) {
    // Create jQuery object for input element
    var $input = jQuery(input).attr("autocomplete", "off");
 
+   jQuery(window).load(function () {
+	   $input.blur(); 
+	});
+   
    // Apply inputClass if necessary
    if (options.inputClass) $input.addClass(options.inputClass);
 
