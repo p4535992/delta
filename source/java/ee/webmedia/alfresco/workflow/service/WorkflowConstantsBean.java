dfe77fad94a18d6b228bff7a617b011e27628f29
aleksandr.brukvin <aleksandr.brukvin@X10505.just.sise>
2019-02-20 10:03:09 +0200

    DELTA-1430 fix

--- source/java/ee/webmedia/alfresco/workflow/service/WorkflowConstantsBean.java
+++ source/java/ee/webmedia/alfresco/workflow/service/WorkflowConstantsBean.java
@@ -134,7 +134,7 @@ public class WorkflowConstantsBean implements InitializingBean {
     }
 
     public boolean isDocumentWorkflowEnabled() {
-        return documentWorkflowEnabled || independentWorkflowEnabled;
+        return documentWorkflowEnabled;
     }
 
     public void setDocumentWorkflowEnabled(boolean documentWorkflowEnabled) {
